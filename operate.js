b6f3f659d4c10e4e0a8bf32cc5124402436e3975
wangkai15 <wangkai15@100tal.com>
2019-07-14 10:25:06 +0800

    feat: add mongodb

---
+++ operate.js
@@ -0,0 +1,44 @@
+const mongoose = require('./db')
+const Schema = mongoose.Schema;
+
+const ceshiSchema = new Schema({
+  name: String,
+  website: String,
+  phone: String,
+  tag: Array,
+});
+
+const MyModel = mongoose.model('data', ceshiSchema);
+
+
+class Mongodb {
+  constructor () {
+
+  }
+// 查询
+  query () {
+     return new Promise((resolve, reject) => {
+       MyModel.find({}, (err, res) => {
+         if(err) {
+           reject(err)
+         }
+         resolve(res)
+       })
+     })
+  }
+// 保存
+  save (obj) {
+     const m = new MyModel(obj)
+     return new Promise((resolve, reject)=> {
+       m.save((err, res) => {
+         if (err) {
+           reject(err)
+         }
+         resolve(res)
+         console.log(res)
+       })
+     })
+     
+  }
+}
+module.exports = new Mongodb()
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var koa = require('koa');
-var app = new koa();
 var router = require('koa-router')();
+const ModelDb = require('./operate')
+var bodyParser = require('koa-bodyparser');
+var app = new koa();
+
+app.use(bodyParser());
+
+
+
 // CORS是一个W3C标准，全称是"跨域资源共享"（Cross-origin resource sharing）。
 // 下面以koa2-cors为例，
 const cors = require('koa2-cors');
@@ -20,10 +27,25 @@ app.use(cors({
     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }))
 
-router.get('/koaapi', async function (ctx) {
+router.post('/koaapi', async function (ctx) {
+    const obj =ctx.request.body
+     await ModelDb.save(obj)
+    let data = await ModelDb.query()
+    // ctx.response.body = data
+
     ctx.res.statusCode = '100'
     ctx.res.message = 'ok'
-    ctx.body = {'name': 'wk', 'status': ctx.res.statusCode, 'message':ctx.res.message}
+    ctx.body = {'data': data, 'status': ctx.res.statusCode, 'message':ctx.res.message}
+});
+router.get('/koaList', async function (ctx) {
+    let data = await ModelDb.query()
+    console.log(data, 333)
+
+    // ctx.response.body = data
+
+    ctx.res.statusCode = '100'
+    ctx.res.message = 'ok'
+    ctx.body = {'data': data, 'status': ctx.res.statusCode, 'message':ctx.res.message}
 });
 
 app
