bd07ff2d82f4289adaeaf9fdeb0b1fa1fe31582d
wangkai15 <wangkai15@100tal.com>
2019-07-18 11:46:29 +0800

    add

---
+++ middleware/errorHandle.js
@@ -0,0 +1,11 @@
+ var errorHandle = (ctx, next) => {
+    return next().catch((err) => {
+      if (401 == err.status) {
+        ctx.status = 401;
+        ctx.body = 'Protected resource, use Authorization header to get access\n';
+      } else {
+        throw err;
+      }
+    })
+}
+module.exports = errorHandle
--- server.js
+++ server.js
@@ -2,16 +2,26 @@ var koa = require('koa');
 var bodyParser = require('koa-bodyparser');
 var app = new koa();
 const routers = require('./router')
+var errorHandle = require('./middleware/errorHandle')
+var koajwt = require('koa-jwt');
+
 
 // CORS是一个W3C标准，全称是"跨域资源共享"（Cross-origin resource sharing）。
 const cors = require('koa2-cors');
 
 app
-    .use(bodyParser())
     .use(cors())
+    .use(bodyParser())
+
+    .use(errorHandle) // Custom 401 handling if you don't want to expose koa-jwt errors to users   
+    .use(koajwt({secret: 'my_token'}).unless({
+        path: [/\/koaapi/, /\/login/],
+      })) // Middleware below this line is only reached if JWT token is valid
+
     .use(routers.routes())
-    .use(routers.allowedMethods());
-    
- app.listen(4000, () => {
+    .use(routers.allowedMethods())
+
+
+app.listen(4000, () => {
     console.log('sever is running at 4000')
 });
